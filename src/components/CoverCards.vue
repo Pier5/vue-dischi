86f3a1e1775f8072983d5883c0f0018648ac9ecd
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-24 17:23:33 +0100

    -aggiustate immagini-

--- src/components/CoverCards.vue
+++ src/components/CoverCards.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="d-flex justify-content-center pt-2">
+  <div class="d-flex justify-content-center pt-4">
     <div class="cards text-center">
-      <img class="w-100" :src="CharacterData.poster" :alt="CharacterData.title">
+      <img class="img-cover pt-2" :src="CharacterData.poster" :alt="CharacterData.title">
       <h2 class="text-white fs-5 p-2">{{ CharacterData.title }}</h2>
       <h3 class="fs-5 pt-1">{{ CharacterData.author }}</h3>
       <h4 class="fs-5">{{ CharacterData.year }}</h4>
@@ -22,6 +22,9 @@ export default {
 <style scoped lang="scss">
   .cards {
     width: 150px;
-    background-color: gray
+    background-color: gray;
+    .img-cover {
+      width: 130px;
+    }
   }
 </style>
