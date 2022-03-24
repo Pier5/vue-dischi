b427bea60d4da824277e018e185bdf870eb628a9
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-24 17:38:51 +0100

    -loading-

--- src/components/MainSpoty.vue
+++ src/components/MainSpoty.vue
@@ -1,7 +1,8 @@
 <template>
   <main class="bg-dark">
     <div class="container">
-      <div class="row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 d-flex align-items-stretch flex-wrap">
+      <div v-if="arrCovers == null" clasS="text-center text-white p-5">Loading..</div>
+      <div v-else class="row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 d-flex align-items-stretch flex-wrap">
         <CoverCards
           v-for="card in arrCovers"
           :key="card.title"
@@ -21,18 +22,19 @@ export default {
   name: 'MainSpoty',
   data () {
     return {
-      arrCovers: []
+      arrCovers: null
     }
   },
   components: {
     CoverCards
   },
   created () {
-    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-      .then((response) => {
-        this.arrCovers = response.data.response
-        console.log(response.data.response)
-      })
+    setTimeout(() => {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((response) => {
+          this.arrCovers = response.data.response
+        })
+    }, 2000)
   }
 }
 </script>
