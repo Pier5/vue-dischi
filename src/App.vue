3cff4f0c69f9bb448d270db27dde917965fb2744
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-24 15:16:46 +0100

    -base-

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderSpoty />
+    <MainSpoty />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import HeaderSpoty from './components/HeaderSpoty.vue'
+import MainSpoty from './components/MainSpoty.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderSpoty,
+    MainSpoty
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/styles/style.scss';
 </style>
---
+++ src/components/HeaderSpoty.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>ciao</div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderSpoty'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
